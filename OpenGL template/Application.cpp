72adbc643a5e290425c911b8b5c06e55c6065da8
MindM <maindi66@gmail.com>
2019-01-27 14:22:25 +0200

    Basic setup done

--- OpenGL template/Application.cpp
+++ OpenGL template/Application.cpp
@@ -1,6 +1,65 @@
-#include <GLFW\glfw3.h>
+#include <glad/glad.h> 
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+using namespace std;
+
+// Callback function when window is resized
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+void processInputs(GLFWwindow *window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+}
+
 
 int main() {
+
+	// Setup window hints
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(800, 600, "OpenGLwin", NULL, NULL);
+	if (window == NULL)
+	{
+		cout << "Could not create OpenGL window." << endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	//int gladLoadGL(void);
+	//printf("OpenGL Version %d.%d loaded", GLVersion.major, GLVersion.minor);
+
+	// Initialize GLAD
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "Failed to initialize GLAD." << endl;
+		return -1;
+	}
+
+	glViewport(0, 0, 800, 600);
+	
+	while (!glfwWindowShouldClose(window)) 
+	{
+		processInputs(window);
+
+		// Render code here
+		glClearColor(0.0f, 0.7f, 0.2f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+
+	}
+
+	glfwTerminate();
 
 	return 0;
 }
